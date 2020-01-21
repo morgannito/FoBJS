f04fc6c536628805052ca1e66f67186ab7f075dd
Th3OnlyC0D3R <nico.fengler@gmx.de>
2020-01-21 18:19:29 +0100

    Update FoBFunctions.js

--- module/FoBFunctions.js
+++ module/FoBFunctions.js
@@ -14,13 +14,17 @@ function ExecuteMoppelAll(fList, nList, cmList){
 
     ConsoleWin.webContents.send('print', "Do: Motivate all Clanmember (Count: "+ClanMemberList.length+")");
     var done = 0;
-    ClanMemberList.forEach(Member => {
-        setTimeout(()=>{
-            FoBCore.DoMotivate(Member.key)
+    
+    var i = 0;
+	var interval = setInterval(function(){
+	    var Member = ClanMemberList[i];
+		FoBCore.DoMotivate(Member.key)
             .then( body =>{
                 var result = processer.GetMotivateResult(body);
                 ConsoleWin.webContents.send('print', `${Member.item["name"]}: ${result}`);
             });
-        }, 1500);
-    });
+	    i++;
+		if(i === ClanMemberList.length)
+			clearInterval(interval);
+	}, 2000);
 }
