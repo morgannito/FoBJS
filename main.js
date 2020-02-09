0099c2d5c2dd394192e995cac9ae32d6fddfe3a4
Th3OnlyC0D3R <nico.fengler@gmx.de>
2020-02-09 11:13:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -478,8 +478,8 @@ function PrepareInfoMenu() {
         for (let i = 0; i < maxLength; i++) {
             var localContent = buildContent;
             const Hidden = visHidden[i];
-            var prod = dList[i];
-            var count = dList[i];
+            var prod = dList[i].prod;
+            var count = dList[i].count;
 
             if (undefined === Hidden || null === Hidden) {
                 localContent = localContent
